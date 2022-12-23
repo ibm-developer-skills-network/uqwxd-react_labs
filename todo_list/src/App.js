2ea998e8935e609554f47aa5bc7a26f83eb5eaa2
Jay <scottjay@msu.edu>
2022-12-22 23:11:09 -0500

    formatting

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./App.css";
+
 const App = () => {
-  const [todos, setTodos] = React.useState([]);
-  const [todo, setTodo] = React.useState("");
-return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
-</div>
-);
+    const [todos, setTodos] = React.useState([]);
+    const [todo, setTodo] = React.useState("");
+    return(
+        <div className ="App">
+        <h1>Todo List</h1>
+        <form>
+        <input type ="text" align ="right" />
+        <button type ="submit">Add Todo</button>
+        </form>
+        </div>
+    );
 };
+
 export default App;
