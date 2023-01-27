75b79c3495e5064402def6205845d4f0ab00ab7e
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-01-27 16:07:20 +0530

    Update App.js

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -3,6 +3,19 @@ import "./App.css";
 const App = () => {
   const [todos, setTodos] = React.useState([]);
   const [todo, setTodo] = React.useState("");
+  
+  // Add the handlesubmit code here
+  
+  
+  // Add the deleteToDo code here
+
+  
+  // Add the toggleComplete code here
+
+  
+  // Add the submitEdits code here
+
+  
 return(
 <div className ="App">
 <h1>Todo List</h1>
