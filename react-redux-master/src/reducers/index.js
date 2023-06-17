659c10a7826daf19edfe268119a39946b28472bc
Alvin Sanchez <alvin.sanchez27@gmail.com>
2023-06-17 19:28:23 -0400

    Adding increment counter using redux

---
+++ react-redux-master/src/reducers/index.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux'
+
+const counter = (state=0,action)=>{
+    if(action.type === "INCREMENT"){
+        return state+action.inc;
+    }
+    return state;
+}
+
+const myReducers = combineReducers({counter});
+
+export default myReducers;
