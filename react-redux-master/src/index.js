28dcfa7ca46c7fc4b75818dd873a95e9e649ca96
23BThomas <bettysamthomas@outlook.com>
2023-07-05 07:56:29 -0400

    Created increment counter app using Redux in React
    Signed-off-by: 23BThomas <bettysamthomas@outlook.com>

--- react-redux-master/src/index.js
+++ react-redux-master/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-import {createStore} from 'redux';
 import {Provider} from 'react-redux'
 import myReducers from './reducers'
+import {legacy_createStore as createStore} from 'redux';;
 
 
 //Create the store
