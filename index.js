1281ac214c11c78bc65a955aebbc86ab0163a01d
Vedanti Bagade <vedanti.r.bagade@gmail.com>
2023-10-07 23:02:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { legacy_createStore as createStore } from 'redux';
+import {Provider} from 'react-redux'
+import myReducers from './reducers'
+
+
+//Create the store
+const myStore = createStore(myReducers);
+
+//This will console log the current state everytime the state changes
+myStore.subscribe(()=>console.log(myStore.getState()));
+
+//Enveloping the App inside the Provider, ensures that the states in the store are available
+//throughout the application
+ReactDOM.render(<Provider store={myStore}><App/></Provider>, document.getElementById('root'));
