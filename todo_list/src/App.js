82efd42659e392640f3886f60fcdc0bca228ab51
Manuel Alejandro Londoño Cadavid <malondonoc@gmail.com>
2023-10-30 20:08:52 -0500

    update App.js

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,28 +1,52 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 const App = () => {
   const [todos, setTodos] = useState([]);
-  
+
   // Add the handlesubmit code here
-  
-  
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let todo = document.getElementById('todoAdd').value
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+    if (newTodo.text.length > 0) {
+      setTodos([...todos].concat(newTodo));
+    } else {
+
+      alert("Enter Valid Task");
+    }
+    document.getElementById('todoAdd').value = ""
+  }
+
   // Add the deleteToDo code here
 
-  
+
   // Add the toggleComplete code here
 
-  
+
   // Add the submitEdits code here
 
-  
-return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" id= 'todoAdd'/>
-<button type ="submit">Add Todo</button>
-</form>
-</div>
-);
+
+  return (
+    <div id="todo-list">
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id='todoAdd'
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      {todos.map((todo) =>
+        <div className="todo" key={todo.id}>
+          <div className="todo-text">{todo.text}</div>
+          {/* insert delete button below this line */}
+        </div>)}
+    </div>
+  );
 };
 export default App;
