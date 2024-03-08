29e25bcb3e8246b7ba0529580747e9d873efa3e6
Renij Shrestha <renijshrestha8@gmail.com>
2024-03-07 20:41:31 -0500

    changes made from theia lab env.

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,27 +1,111 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 const App = () => {
   const [todos, setTodos] = useState([]);
+  const [todoEditing, setTodoEditing] = useState(null);
+
+  useEffect(() => {
+    const json = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(json);
+    if(loadedTodos){
+        setTodos(loadedTodos);
+    }
+  }, [])
+
+  useEffect(() => {
+    if(todos.length > 0) {
+        const json = JSON.stringify(todos);
+        localStorage.setItem("todos", json)
+    }
+  }, [todos])
   
   // Add the handlesubmit code here
+  function handlesubmit(e){
+    e.preventDefault();
+
+    let todo = document.getElementById('todoAdd').value;
+    const newTodo = {
+        id: new Date().getTime(),
+        text: todo.trim(),
+        completed: false,
+    };
+
+    if(newTodo.text.length > 0){
+        setTodos([...todos].concat(newTodo));
+    } else {
+        alert("Enter Valid Task");
+    }
+
+    document.getElementById('todoAdd').value = ""
+
+  }
   
   
   // Add the deleteToDo code here
 
+function deleteTodo(id){
+    let upadtedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(upadtedTodos)
+}
+
   
   // Add the toggleComplete code here
+  function toggleCompleted(id){
+    let upadtedTodos = [...todos].map((todo) => {
+        if(todo.id === id){
+            todo.completed = !todo.completed;
+        }
+        return todo;
+    })
+    setTodos(upadtedTodos);
+  }
 
   
   // Add the submitEdits code here
+  function submitEdits(newtodo){
+    const updatedTodos = [...todos].map((todo) => {
+        if(todo.id === newtodo.id){
+            todo.text = document.getElementById(newtodo.id).value;
+        }
+        return todo;
+    });
+    setTodos(updatedTodos);
+    setTodoEditing(null);
+  }
 
   
 return(
-<div className ="App">
+<div className ="App" id="todo-list">
 <h1>Todo List</h1>
-<form>
+<form onSubmit={handlesubmit}>
 <input type ="text" align ="right" id= 'todoAdd'/>
 <button type ="submit">Add Todo</button>
 </form>
+{todos.map((todo) => {
+    return (
+    <div className="todo" key={todo.id}>
+        <div className="todo-text">
+            <input type="checkbox" id="completed" checked={todo.completed} onChange={() => toggleCompleted(todo.id)}/>
+            {todo.id === todoEditing ?
+            (<input type="text" id = {todo.id} defaultValue={todo.text}/>) :
+            (<div>{todo.text} </div>)
+            }
+        </div>
+        <div className="todo-actions">
+            {todo.id === todoEditing ? 
+            (
+                <button onClick={() => submitEdits(todo)}>Submit Edits</button>
+            ) : 
+        (
+            <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
+        )}
+        <button onClick={() => deleteTodo(todo.id)}>
+            Delete 
+        </button>
+        </div>
+    </div>
+    )
+})}
 </div>
 );
 };
